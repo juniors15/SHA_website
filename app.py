9290f9fc74ffc96148fcc7718122646f38b1abca
juniors15 <a.foucqueteau@mimetas.com>
2017-09-16 13:33:03 +0200

    add the picture and the app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+english_bot.set_trainer(ChatterBotCorpusTrainer)
+english_bot.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html",)
+
+@app.route("/get/<string:query>")
+def get_raw_response(query):
+	
+    
+
+    answer=str(english_bot.get_response(query))
+    return render_template("index.html",answer=answer)
+
+
+
+
+if __name__ == "__main__":
+    app.run()
