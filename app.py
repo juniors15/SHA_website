b566d654f5e3692e9698058f549cec7df37159ee
juniors15 <a.foucqueteau@mimetas.com>
2017-09-16 14:48:14 +0200

    move the stuff to static plus html and app change now the bot work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
@@ -8,19 +8,24 @@ english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStor
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
-
+textDum = ""
 
 @app.route("/")
 def home():
-    return render_template("index.html",)
+    return render_template("index.html",answer=textDum)
 
-@app.route("/get/<string:query>")
+
+@app.route("/get/<string:query>/")
 def get_raw_response(query):
-	
-    
+    answeeer=str(english_bot.get_response(query))
+    if "TV" in query:
+    	answeeer="Do you want to buy an Xbox"
 
-    answer=str(english_bot.get_response(query))
-    return render_template("index.html",answer=answer)
+    if "buy" in query:
+        answeeer="💸💳"
+
+
+    return render_template("index.html",answer=answeeer)
 
 
 
